af6a9de18d4cb95c7d38dc6271ed9feb88af28e5
西帝 <486772+sidi@users.noreply.github.com>
2024-12-27 12:21:50 +0000

    Change modesl to a directory

--- src/prj/myapp/views.py
+++ src/prj/myapp/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
-from . import models
+from .models.module import *
 
 # Create your views here.
 
 class ModuleListView(ListView):
-    model = models.Module 
+    model = Module 
 
 class ModuleDetailView(DetailView):
-    model = models.Module 
+    model = Module 
 
 
 class ModuleCreateView(CreateView):
-    model = models.Module 
+    model = Module 
     fields = '__all__'
     success_url = reverse_lazy('module_list')
 
 class ModuleDeleteView(DeleteView):
-    model = models.Module 
+    model = Module 
     success_url = reverse_lazy('module_list')
 
 class ModuleUpdateView(UpdateView):
-    model = models.Module 
+    model = Module 
     fields = '__all__'
     success_url = reverse_lazy('module_list')
 
